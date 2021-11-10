ecaccb671985a953a7b580d13ec92806a6b41e55
Daniel Kuroski <daniel.kuroski@gmail.com>
2021-11-10 14:35:22 +0100

    Test API by using TaskEither instead of Task

--- framework/hackernews.ts
+++ framework/hackernews.ts
@@ -1,11 +1,15 @@
 import { pipe } from "fp-ts/lib/function";
 import * as TE from "fp-ts/lib/TaskEither";
-import * as T from "fp-ts/lib/Task";
 import * as ROA from "fp-ts/lib/ReadonlyArray";
 import * as ROT from "fp-ts/lib/ReadonlyTuple";
 import * as L from "./List";
 import { getStory, ItemStory } from "./Item";
-import { error } from "fp-ts/lib/Console";
+import { FetchError } from "./fetch";
+
+export type TailFn = TE.TaskEither<
+  FetchError,
+  readonly [readonly ItemStory[], () => TailFn]
+>;
 
 export const topStories = (pageSize = 20) => {
   function tailFn(lists: L.List, itemsAcc: readonly ItemStory[]) {
@@ -15,27 +19,22 @@ export const topStories = (pageSize = 20) => {
       ROT.mapFst((pageListIds) =>
         pipe(
           TE.sequenceArray(pageListIds.map(getStory)),
-          TE.map((items) => pipe(itemsAcc, ROA.concat(items))),
-          TE.fold((e) => {
-            error(e)();
-            return T.of(ROA.empty);
-          }, T.of)
+          TE.map((items) => pipe(itemsAcc, ROA.concat(items)))
         )
       )
     );
 
     return pipe(
       items,
-      T.map((result) => [result, () => tailFn(remainingList, result)] as const)
+      TE.map(
+        (result) =>
+          [result, (): TailFn => tailFn(remainingList, result)] as const
+      )
     );
   }
 
   return pipe(
     L.getLists,
-    TE.fold((e) => {
-      error(e)();
-      return T.of(L.empty);
-    }, T.of),
-    T.chain((lists) => tailFn(lists, ROA.empty))
+    TE.chain((lists) => tailFn(lists, ROA.empty))
   );
 };
