ac4d7f917857221c2bc66cb04fde3702dbe3e178
Daniel Kuroski <daniel.kuroski@gmail.com>
2021-12-22 22:05:01 +0100

    Start tests on vue-hook

--- packages/vue/src/__tests__/index.ts
+++ packages/vue/src/__tests__/index.ts
@@ -1,4 +1,3 @@
-import { act, renderHook } from "@testing-library/react-hooks";
 import { useStories } from "..";
 import * as RD from "@devexperts/remote-data-ts";
 import * as O from "fp-ts/Option";
@@ -8,6 +7,16 @@ import {
   mockSuccessListRequest,
 } from "@kuroski-hackernews/mocks";
 import { pipe } from "fp-ts/function";
+import { nextTick } from "vue-demi";
+
+const scheduler =
+  typeof setImmediate === "function" ? setImmediate : setTimeout;
+
+function flushPromises() {
+  return new Promise(function (resolve) {
+    scheduler(resolve, 1000);
+  });
+}
 
 describe("Hooks", () => {
   test("useStories", async () => {
@@ -21,98 +30,98 @@ describe("Hooks", () => {
       }))
     );
 
-    const { waitForNextUpdate, result } = renderHook(() => useStories(1));
+    const { remoteData, stories, loadMore } = useStories(1);
 
-    expect(result.current.stories).toHaveLength(0);
-    expect(result.current.remoteData).toBe(RD.pending);
-    expect(result.current.loadMore).toBe(O.none);
+    expect(stories.value).toHaveLength(0);
+    expect(remoteData.value).toEqual(RD.pending);
+    expect(loadMore.value).toEqual(O.none);
 
-    await waitForNextUpdate();
+    // await flushPromises();
+    // await flushPromises();
+    // await nextTick();
 
-    expect(result.current.stories).toHaveLength(1);
-    expect(result.current.stories).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_tag": "ItemStory",
-          "by": "dhouston",
-          "id": 76686866,
-          "kids": Array [],
-          "text": Object {
-            "_tag": "None",
-          },
-          "title": Object {
-            "_tag": "Some",
-            "value": "My story 76686866",
-          },
-          "url": Object {
-            "_tag": "Some",
-            "value": "http://my-url.com/?storyId=76686866",
-          },
-        },
-      ]
-    `);
-    expect(result.current.remoteData).toEqual(
-      RD.success(result.current.stories)
-    );
-    expect(O.isSome(result.current.loadMore)).toBeTruthy();
+    // expect(stories.value).toHaveLength(1);
+    // expect(stories.value).toMatchInlineSnapshot(`
+    //   Array [
+    //     Object {
+    //       "_tag": "ItemStory",
+    //       "by": "dhouston",
+    //       "id": 76686866,
+    //       "kids": Array [],
+    //       "text": Object {
+    //         "_tag": "None",
+    //       },
+    //       "title": Object {
+    //         "_tag": "Some",
+    //         "value": "My story 76686866",
+    //       },
+    //       "url": Object {
+    //         "_tag": "Some",
+    //         "value": "http://my-url.com/?storyId=76686866",
+    //       },
+    //     },
+    //   ]
+    // `);
+    // expect(remoteData.value).toEqual(RD.success(stories.value));
+    // expect(O.isSome(loadMore.value)).toBeTruthy();
 
-    const loadMore = pipe(
-      result.current.loadMore,
-      O.getOrElseW(() => {
-        throw new Error("Load more must exist in this test");
-      })
-    );
+    // const loadMore = pipe(
+    //   result.current.loadMore,
+    //   O.getOrElseW(() => {
+    //     throw new Error("Load more must exist in this test");
+    //   })
+    // );
 
-    act(() => {
-      loadMore();
-    });
+    // act(() => {
+    //   loadMore();
+    // });
 
-    expect(result.current.remoteData).toBe(RD.pending);
+    // expect(result.current.remoteData).toBe(RD.pending);
 
-    await waitForNextUpdate();
+    // await waitForNextUpdate();
 
-    expect(result.current.stories).toHaveLength(2);
-    expect(result.current.stories).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "_tag": "ItemStory",
-          "by": "dhouston",
-          "id": 76686866,
-          "kids": Array [],
-          "text": Object {
-            "_tag": "None",
-          },
-          "title": Object {
-            "_tag": "Some",
-            "value": "My story 76686866",
-          },
-          "url": Object {
-            "_tag": "Some",
-            "value": "http://my-url.com/?storyId=76686866",
-          },
-        },
-        Object {
-          "_tag": "ItemStory",
-          "by": "dhouston",
-          "id": 67829238,
-          "kids": Array [],
-          "text": Object {
-            "_tag": "None",
-          },
-          "title": Object {
-            "_tag": "Some",
-            "value": "My story 67829238",
-          },
-          "url": Object {
-            "_tag": "Some",
-            "value": "http://my-url.com/?storyId=67829238",
-          },
-        },
-      ]
-    `);
-    expect(result.current.remoteData).toEqual(
-      RD.success(result.current.stories)
-    );
-    expect(O.isNone(result.current.loadMore)).toBeTruthy();
+    // expect(result.current.stories).toHaveLength(2);
+    // expect(result.current.stories).toMatchInlineSnapshot(`
+    //   Array [
+    //     Object {
+    //       "_tag": "ItemStory",
+    //       "by": "dhouston",
+    //       "id": 76686866,
+    //       "kids": Array [],
+    //       "text": Object {
+    //         "_tag": "None",
+    //       },
+    //       "title": Object {
+    //         "_tag": "Some",
+    //         "value": "My story 76686866",
+    //       },
+    //       "url": Object {
+    //         "_tag": "Some",
+    //         "value": "http://my-url.com/?storyId=76686866",
+    //       },
+    //     },
+    //     Object {
+    //       "_tag": "ItemStory",
+    //       "by": "dhouston",
+    //       "id": 67829238,
+    //       "kids": Array [],
+    //       "text": Object {
+    //         "_tag": "None",
+    //       },
+    //       "title": Object {
+    //         "_tag": "Some",
+    //         "value": "My story 67829238",
+    //       },
+    //       "url": Object {
+    //         "_tag": "Some",
+    //         "value": "http://my-url.com/?storyId=67829238",
+    //       },
+    //     },
+    //   ]
+    // `);
+    // expect(result.current.remoteData).toEqual(
+    //   RD.success(result.current.stories)
+    // );
+    // expect(O.isNone(result.current.loadMore)).toBeTruthy();
   });
 });
